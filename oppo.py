e7e68cf50414dcd8d79f3353cb9fe9061e938217
jacobi <jocbi2017>
2018-05-21 03:41:50 -0700

    update

--- oppo.py
+++ oppo.py
@@ -16,7 +16,7 @@ import datetime
 import logging
 import configue
 class Oppo_spider:
-    #.......##################
+    #.123131......#######################################123
     def __init__(self):
 
         self.s = requests.session()
